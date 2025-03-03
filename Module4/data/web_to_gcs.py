79b5ff5fc5f1983d2ef388fc9be37197f0473203
tyeejian <yeejian_tee02@yahoo.com>
2025-03-03 16:28:52 +0800

    Added homework

---
+++ Module4/data/web_to_gcs.py
@@ -0,0 +1,64 @@
+import io
+import os
+import requests
+import pandas as pd
+from google.cloud import storage
+from io import BytesIO
+
+"""
+Pre-reqs: 
+1. `pip install pandas pyarrow google-cloud-storage`
+2. Set GOOGLE_APPLICATION_CREDENTIALS to your project/service-account key
+3. Set GCP_GCS_BUCKET as your bucket or change default value of BUCKET
+"""
+
+# services = ['fhv','green','yellow']
+# https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2019-01.csv.gz
+init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/'
+# switch out the bucketname
+BUCKET = os.environ.get("GCP_GCS_BUCKET", "zoomcamp-bucket-354966599360")
+
+
+def upload_to_gcs(bucket, object_name, local_file):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    """
+    # # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    # storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    # storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+
+
+def web_to_gcs(year, service):
+    for i in range(12):
+        
+        # sets the month part of the file_name string
+        month = '0'+str(i+1)
+        month = month[-2:]
+
+        # csv file_name
+        file_name = f"{service}_tripdata_{year}-{month}.csv.gz"
+
+        # download it using requests via a pandas df
+        request_url = f"{init_url}/{service}/{file_name}"
+        r = requests.get(request_url)
+        open(file_name, 'wb').write(r.content)
+        print(f"Local: {file_name}")
+
+
+        # upload it to gcs 
+        upload_to_gcs(BUCKET, f"{service}_new/{file_name}", file_name)
+        print(f"GCS: {service}_new/{file_name}")
+
+
+# web_to_gcs('2019', 'green')
+# web_to_gcs('2020', 'green')
+# web_to_gcs('2019', 'yellow')
+# web_to_gcs('2020', 'yellow')
+web_to_gcs('2019', 'fhv')
+# web_to_gcs('2020', 'fhv')
